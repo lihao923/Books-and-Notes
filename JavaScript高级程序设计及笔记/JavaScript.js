53d302b27363bb729e253a7bf8b32094b6404fb1
lihao923 <lihao923@126.com>
2019-12-14 20:22:35 +0800

    read book 20191214 20:22

--- JavaScript高级程序设计及笔记/JavaScript.js
+++ JavaScript高级程序设计及笔记/JavaScript.js
@@ -1222,15 +1222,43 @@ for(var i = 0, len = mods.length; i < len; i++) {
 */
 
 
+/*
+*
+* 第21章 Ajax 与 Comet
+*
+*/
+
+function createXHR() {
+	if(typeof XMLHttpRequest != 'undefined') {
+		return new XMLHttpRequest();
+	} else if(typeof ActiveXObject != 'undefined') {
+		if(typeof arguments.callee.activeXString != 'string') {
+			var versions = ['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp'],
+				i, len;
+			for(i = 0; len = versions.length; i < len; i++) {
+				try {
+					new ActiveXObject(versions[i]);
+					arguments.callee.activeXString = versions[i];
+					break;
+				} catch(ex) {
+					// 跳过
+				}
+			}
+		}
+		return new ActiveXObject(arguments.callee.activeXString);
+	} else {
+		throw new Error('No XHR object available.');
+	}
+}
+
+var xhr = createXHR();
 
 
-
-
-
-
-
-
-
+// addURLParam(url, name, value) {
+	url += url.indexOf('?') == -1 ? '?' : '&';
+	url += encodeURIComponent(name) + '=' + encodeURIComponent(value);
+	return url;
+}
 
 
 
