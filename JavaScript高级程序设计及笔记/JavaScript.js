5350e138e6f10e929c13cc0142c554e2219d6b75
lihao923 <lihao923@126.com>
2019-12-16 19:38:38 +0800

    read book 20191216 19:38

--- JavaScript高级程序设计及笔记/JavaScript.js
+++ JavaScript高级程序设计及笔记/JavaScript.js
@@ -1260,15 +1260,47 @@ var xhr = createXHR();
 	return url;
 }
 
+// 使用XHR对象实现HTTP流的典型代码
+function createStreamingClient(url, progress, finished) {
+	var xhr = new XMLHttpRequest(),
+		received = 0;
+
+	xhr.open('get', url, true);
+	xhr.onreadystatechange = function() {
+		var result;
+
+		if(xhr.readyState == 3) {
+			// 只取得最新数据并调整计数器
+			result = xhr.responseText.substring(received);
+			received += result.length;
+
+			// 调用progress回调函数
+			progress(result);
+		} else if(xhr.readyState == 4) {
+			finished(xhr.responseText);
+		}
+	};
+
+	xhr.send(null);
+	return xhr;
+}
+
+var client = createStreamingClient('streaming.php', function(data) {
+	alert('Received:' + data);
+}, function(data) {
+	alert('Done!');
+})
 
 
 
 
 
 
-
-
-
+/*
+*
+* 第22章 高级技巧
+*
+*/
 
 
 
