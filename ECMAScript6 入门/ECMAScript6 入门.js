fd15805977f9bfa75e2a89eba0286f705fb067a3
lihao923 <lihao923@126.com>
2019-12-18 20:22:14 +0800

    read book ECMAScript6 入门 20191218 20:22

--- ECMAScript6 入门/ECMAScript6 入门.js
+++ ECMAScript6 入门/ECMAScript6 入门.js
@@ -866,9 +866,86 @@ console.log(tmpl(data));
 let func = (name) => `Hello ${name}!`;
 func('Jack') // 'Hello Jack!'
 
-
-
 /* 6.实例：模板编译 */
+
+//
+let template = `
+<ul>
+	<% for(let i = 0; i < data.supplies.length; i++) { %>
+		<li><%= data.supplies[i] %></li>
+	<% } %>
+</ul>
+`;
+
+// 
+echo('<ul>');
+for(let i = 0; i < data.supplies.length; i++) {
+	echo('<li>');
+	echo(data.supplies[i]);
+	echo('</li>');
+};
+echo('</ul>');
+
+//
+let evalExpr = /<%= (.+?)%>/g;
+let expr = /<%([\s\S]+?)%>/g;
+template = template
+	.replace(evalExpr, '`); \n echo( $1); \n echo(`')
+	.replace(expr, '`); \n $1 \n echo(`');
+
+template = 'echo(`' + template + '`);';
+
+// 
+let script = 
+`(function parse(data) {
+	let output = '';
+	function echo(html) {
+		output += html;
+	}
+
+	${ template }
+
+	return output;
+})`;
+
+return script;
+
+// 
+function compile(template) {
+	const evalExpr = /<%= (.+?)%>/g;
+	const expr = /<%([\s\S]+?)%>/g;
+
+	template = template
+		.replace(evalExpr, '`); \n echo( $1 ); \n echo(`')
+		.replace(expr, '`); \n $1 \n echo(`');
+
+	template = 'echo(`' + template +'`);';
+
+	let script = 
+	`(function parse(data) {
+		let output = '';
+
+		function echo(html) {
+			output += html;
+		}
+
+		${ template }
+
+		return output;
+	})`
+
+	return script;
+}
+
+// 
+let parse = eval(compile(template));
+div.innerHTML = parse({ supplies: ['broom', 'mop', 'cleaner'] });
+//   <ul>
+//     <li>broom</li>
+//     <li>mop</li>
+//     <li>cleaner</li>
+//   </ul>
+
 /* 7.标签模板 */
 /* 8.模板字符串的限制 */
 
