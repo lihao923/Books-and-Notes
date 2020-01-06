15c809dcb82f213c9e7f2bf91b77e076f9c1b70a
lihao923 <lihao923@126.com>
2020-01-06 19:45:38 +0800

    read book ECMAScript 6 入门

--- ECMAScript6 入门/ECMAScript6 入门.js
+++ ECMAScript6 入门/ECMAScript6 入门.js
@@ -2227,6 +2227,346 @@ try {
 
 
 
+/*
+* 第九章 数组的扩展
+*/
+
+
+/* 1.扩展运算符 */
+console.log(...[1, 2, 3])
+// 1 2 3
+
+console.log(1, ...[2, 3, 4], 5)
+// 1 2 3 4 5
+
+[...document.querySelectorAll('div')]
+// [<div>, <div>, <div>] 
+
+
+
+
+function push(array, ...items) {
+	array.push(...items);
+}
+
+function add(x, y) {
+	return x + y;
+}
+
+const numbers = [1, 48];
+add(...numbers)
+
+
+
+function f(v, w, x, y, z) {
+	const args = [0, 1]
+}
+f(-1, ...args, 2, ...[3]);
+
+
+
+const arr = [
+	...(x > 0 ? ['a']:[]),
+	'b',
+];
+
+
+[...[], 1]
+// [1]
+
+(...[1, 2])
+// Uncaught SyntaxError: Unexpected number
+
+console.log((...[1, 2]))
+// Uncaught SyntaxError: Unexpected number
+
+console.log(...[1, 2])
+// 1 2
+
+
+// ES5的写法
+function f(x, y, z) {
+	// ...
+}
+var args = [0, 1, 2];
+f.apply(null, args);
+
+// ES6的写法
+function f(x, y, z) {
+	// ...
+}
+let args = [0, 1, 2];
+f(...args);
+
+
+
+// ES5的写法
+Math.max.apply(null, [14, 3, 77])
+
+// ES6的写法
+Math.max(...[14, 3, 77])
+
+// 等同于
+Math.max(14, 3, 77)
+
+
+
+// ES5的写法
+var arr1 = [0, 1, 2];
+var arr2 = [3, 4, 5];
+Array.prototype.push.apply(arr1, arr2);
+
+// ES6的写法
+let arr1 = [0, 1, 2];
+let arr2 = [3, 4, 5];
+arr1.push(...arr2);
+
+
+// ES5
+new (Date.bind.apply(Date, [null, 2015, 1, 1]))
+// ES6
+new Date(...[2015, 1, 1])
+
+
+const a1 = [1, 2];
+const a2 = a1;
+a2[0] = 2;
+a1 // [2, 2]
+
+// ES5
+const a1 = [1, 2];
+const a2 = a1.concat();
+a2[0] = 2;
+a1 // [1, 2]
+
+// ES6
+const a1 = [1, 2];
+// 写法一
+const a2 = [...a1];
+// 写法二
+const [...a2] = a1;
+
+
+const arr1 = ['a', 'b'];
+const arr2 = ['c'];
+const arr3 = ['d', 'e'];
+
+// ES5的合并数组
+arr1.concat(arr2, arr3);
+// ['a', 'b', 'c', 'd', 'e']
+
+// ES6的合并数组
+[...arr1, ...arr2, ...arr3]
+// ['a', 'b', 'c', 'd', 'e']
+
+
+const a1 = [{foo: 1}]
+const a2 = [{bar: 2}]
+
+const a3 = a1.concat(a2);
+const a4 = [...a1, ...a2];
+
+a3[0] === a1[0] // true
+a4[0] === a1[0] // true
+
+
+//ES5
+a = list[0], rest = list.slice(1)
+// ES6
+[a, ...rest] = list
+
+
+const [first, ...rest] = [1, 2, 3, 4, 5];
+first // 1
+rest // [2, 3, 4, 5]
+
+const [first, ...rest] = []
+first // undefined
+rest // []
+
+const [first, ...rest] = ['foo']
+first // 'foo'
+rest // []
+
+
+const [...butLast, last] = [1, 2, 3, 4, 5]
+// 报错
+const [first, ..middle, last] = [1, 2, 3, 4, 5]
+// 报错
+
+
+[...'hello'] // ['h', 'e', 'l', 'l', 'o']
+
+functin length(str) {
+	return [...str].length;
+}
+length('x\uD83D\uDE80y') // 3
+
+let str = 'x\uD83D\uDE80y';
+str.split('').reverse().join('')
+// 'y\uDE80\uD83Dx'
+
+[...str].reverse().join('')
+// 'y\uDE80\uD83Dx'
+
+
+let nodelist = document.querySelectorAll('div');
+let array = [...nodelist];
+
+Number.prototype[Symbol.iterator] = function*() {
+	let i = 0;
+	let num = this.valueOf();
+	while (i < num) {
+		yield i++;
+	}
+}
+console.log([...5]) // [0, 1, 2, 3, 4]
+
+
+function arrayLike = {
+	'0': 'a',
+	'1': 'b',
+	'2': 'c',
+	length: 3
+};
+
+// TypeError: Cannot spread non-iterable object.
+let arr = [...arrayLike];
+
+let map = new Map([
+	[1, 'one'],
+	[2, 'two'],
+	[3, 'three'],
+]);
+let arr = [...map.keys()]; // [1, 2, 3]
+
+
+const go = function*() {
+	yield 1;
+	yield 2;
+	yield 3;
+};
+[...go()] // [1, 2, 3]
+
+
+const obj = {a: 1, b: 2};
+let arr = [...obj]; // TypeError: Cannot spread non-iterable object
+
+
+
+
+/* 2.Array.from() */
+
+let arrayLike = {
+	'0': 'a',
+	'1': 'b',
+	'2': 'c',
+	length: 3
+};
+// ES5
+var arr1 = [].slice.call(arrayLike); // ['a', 'b', 'c']
+
+// ES6
+let arr2 = Array.from(arrayLike); // ['a', 'b', 'c']
+
+
+// NodeList对象
+let ps = document.querySelectorAll('p');
+Array.from(ps).filter(p => {
+	return p.textContent.length > 100;
+});
+
+// arguments对象
+function foo() {
+	var args = Array.from(arguments)
+}
+
+
+Array.from('hello')
+// ['h', 'e', 'l', 'l', 'o']
+
+let nameSet = new Set(['a', 'b'])
+Array.from(nameSet) // ['a', 'b']
+
+
+
+// arguments对象
+function foo() {
+	const args = [...arguments];
+}
+// NodeList对象
+[...document.querySelectorAll('div')]
+
+
+Array.from({length: 3});
+// [undefined, undefined, undefined]
+
+
+
+const toArray = (() => {
+	Array.from ? Array.from : obj => [].slice.call(obj)
+})();
+
+
+
+Array.from(arrayLike, x => x * x);
+// 等同于
+Array.from(arrayLike).map(x => x * x)
+Array.from([1, 2, 3], (x) => x * x)
+// [1, 4, 9]
+
+
+let spans = document.querySelectorAll('span.name');
+// map()
+let names1 = Array.prototype.map.call(spans, s => s.textContent);
+// Array.from()
+let names2 = Array.from(spans, s => s.textContent)
+
+
+Array.from([1, , 2, , 3], (n) => n || 0)
+// [1, 0, 2, 0, 3]
+
+function typesOf() {
+	return Array.from(arguments, value => typeof value)
+}
+typesOf(null, [], NaN)
+// ['object', 'object', 'number']
+
+
+
+Array.from({length: 2}, () => 'jack')
+// ['jack', 'jack']
+
+
+function countSymbols(string) {
+	return Array.from(string).length;
+}
+
+
+/* 3.Array.of() */
+
+Array.of(3, 11, 8) // [3, 11, 8]
+Array.of(3) // [3]
+Array.of(3).length // 1
+
+
+Array() // []
+Array(3) // [, , ,]
+Array(3, 11, 8) // [3, 11, 8]
+
+
+Array.of() // []
+Array.of(undefined) // [undefined]
+Array.of(1) // [1]
+Array.of(1, 2) // [1, 2]
+
+
+function ArrayOf() {
+	return [].slice.call(arguments);
+}
+
+
+/* 4.数组事例的copyWithin() */
 
 
 
@@ -2237,16 +2577,13 @@ try {
 
 
 
-
-
-
-
-
-
-
-
-
-
+/* 5.数组事例的find()和findIndex() */
+/* 6.数组事例的fill() */
+/* 7.数组事例的entries(), keys()和values() */
+/* 8.数组事例的includes() */
+/* 9.数组事例的flat(), flatMap() */
+/* 10.数组的空位 */
+/* 11.Array.prototype.sort()的排序稳定性 */
 
 
 
