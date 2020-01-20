9e982829465c12283fd2e06b0d5be309c73b7af8
lihao923 <lihao923@126.com>
2020-01-21 00:19:34 +0800

    read book ECMAScript 6 入门

--- ECMAScript6 入门/ECMAScript6 入门.js
+++ ECMAScript6 入门/ECMAScript6 入门.js
@@ -5638,24 +5638,58 @@ run(g);
 
 /* 13.Promise.try() */
 
+Promise.resolve().then(f)
+
+const f = () => console.log('now');
+Promise.resolve().then(f);
+console.log('next');
+// next
+// now
+
+
+const f = () => console.log('now');
+(async () => f())();
+console.log('next');
+// now
+// next
+
+
+(async () => f())().then(...).catch(...)
+
+
+const f = () => console.log('now');
+(() => new Promise(resolve => resolve(f())))();
+console.log('next');
+// now
+// next
+
+
+const f = () => console.log('now');
+Promise.try(f);
+console.log('next');
+// now
+// next
 
 
 
+function getUsername(userId) {
+	return database.users.get({id: userId}).then(function(user) {return user.name});
+}
+
+database.users.get({id: userId}).then(...).catch(...)
+
+try{
+	database.users.get({id: userId})
+		.then(...)
+		.catch(...)
+} catch{
+	// ...
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+Promise.try(() => database.users.get({id: userId}))
+	.then(...)
+	.catch(... )
 
 
 
