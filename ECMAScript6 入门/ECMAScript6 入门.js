db34d0ed9b97af1665be9a773e3b49bd0a6c8871
lihao923 <lihao923@126.com>
2020-01-22 00:20:53 +0800

    read book ECMAScript 6 入门

--- ECMAScript6 入门/ECMAScript6 入门.js
+++ ECMAScript6 入门/ECMAScript6 入门.js
@@ -6045,13 +6045,171 @@ for(let line of readLineSync(fileName)) {
 
 
 
-
-
-
 /* 7.for...of循环 */
 
+const arr = ['red', 'green', 'blue'];
+for(let v of arr) {
+	console.log(v); // red green blue
+}
+const obj = {};
+obj[Symbol.iterator] = arr[Symbol.iterator].bind(arr);
+
+for(let v of obj) {
+	console.log(v); // red green blue
+}
 
 
+const arr = ['red', 'green', 'blue'];
+arr.forEach(function(element, index) {
+	console.log(element); // red green blue
+	console.log(index); // 0 1 2
+})
+
+
+
+var arr = ['a', 'b', 'c', 'd'];
+for(let a in arr) {
+	console.log(a); // 0 1 2 3
+}
+
+for(let a of arr) {
+	console.log(a) // a b c d
+}
+
+
+
+let arr = [3, 5, 7];
+arr.foo = 'hello';
+for(let i in arr) {
+	console.log(i) // '0', '1', '2', 'foo'
+}
+for(let i of arr) {
+	console.log(i); // '3', '5', '7'
+}
+
+
+
+
+var engines = new Set(['Gecko', 'Trident', 'Webkit', 'Webkit']);
+for(var e of engines) {
+	console.log(e);
+}
+// Gecko
+// Trident
+// Webkit
+
+var es6 = new Map();
+es6.set('edition', 6);
+es6.set('committee', 'TC39');
+es6.set('standard', 'ECAM-262');
+for(var [name, value] of es6) {
+	console.log(name + ':' + value);
+}
+// edition: 6
+// committee: TC39
+// standard: ECMA-262
+
+
+let map = new Map().set('a', 1).set('b', 2);
+for(let pair of map) {
+	console.log(pair);
+}
+// ['a', 1]
+// ['b', 2]
+
+for(let [key, value] of map) {
+	console.log(key + ':' + value)
+}
+// a:1
+// b:2
+
+
+let arr = ['a', 'b', 'c'];
+for(let pair of arr.entries()) {
+	console.log(pair);
+}
+// [0, 'a']
+// [1, 'b']
+// [2, 'c']
+
+
+
+
+// 字符串
+let str = 'hello';
+for(let s of str) {
+	console.log(s); // h e l l o
+}
+// DOM NodeList对象
+let paras = document.querySelectorAll('p');
+for(let p of paras) {
+	p.classList.add('test');
+}
+// arguments对象
+function printArgs() {
+	for(let x of arguments) {
+		console.log(x);
+	}
+}
+printArgs('a', 'b')
+// 'a'
+// 'b'
+
+let arrayLike = {length: 2, 0: 'a', 1: 'b'};
+// 报错
+for(let x of arrayLike) {
+	console.log(x);
+}
+// 正确
+for(let x of Array.from(arrayLike)) {
+	console.log(0);
+}
+
+
+
+
+let es6 = {
+	edition: 6,
+	committee: 'TC39',
+	standard: 'ECMA-262'
+};
+for(let e in es6) {
+	console.log(e);
+}
+// edition
+// committee
+// standard
+for(let e of es6) {
+	console.log(e);
+}
+// TypeError: es6[Symbol.iterator] is not a function
+
+
+for(var key of Object.keys(someObject)) {
+	console.log(key + ':' + someObject[key]);
+}
+
+function*entries(obj) {
+	for(let key of Object.keys(obj)) {
+		yield [key, obj[key]];
+	}
+}
+
+for(let [key, value] of entries(obj)) {
+	console.log(key, '->', value);
+}
+// a -> 1
+// b -> 2
+// c -> 3
+
+
+
+for(var n of fibonacci) {
+	if(n > 1000) {
+		break;
+	}
+	console.log(n);
+}
 
 
 
